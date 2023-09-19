1c18fffc7615d3b07d6711d590a8300d1d8bef72
sandhyapokuru <sandhyau1018@gmail.com>
2023-09-19 16:59:44 +0530

    java code

--- A.java
+++ A.java
@@ -3,5 +3,8 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("i am into sandhya folder");
+		System.out.println("i am into sandhya folder");
+		System.out.println("i am into sandhya folder");
+		System.out.println("i am into sandhya folder");
 	}
 }
